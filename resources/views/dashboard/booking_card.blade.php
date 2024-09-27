24f70d6b3f27545f7035e6070111910012086a49
PhortKid <middlephort@gmail.com>
2024-09-27 20:48:15 +0300

    update chache

--- resources/views/dashboard/booking_card.blade.php
+++ resources/views/dashboard/booking_card.blade.php
@@ -21,7 +21,7 @@
 
 <!-- All Booking -->
 <div class="col-xl-3 col-md-6 mb-4">
-    <div class="card border-left-success shadow h-100 py-2">
+    <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
